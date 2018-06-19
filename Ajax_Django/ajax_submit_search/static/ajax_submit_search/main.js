24d381648806919012cd8adcbeb5121364998886
ineffablelucky <kumar.lakshay1991@gmail.com>
2018-06-19 18:14:57 +0530

    add person callback remaining

--- Ajax_Django/ajax_submit_search/static/ajax_submit_search/main.js
+++ Ajax_Django/ajax_submit_search/static/ajax_submit_search/main.js
@@ -1,11 +1,11 @@
 $(document).on('submit','#create_form', function(e){
-    //e.preventDefault();
+    e.preventDefault();
 
     $.post('create/',
      { name : $("#name_id").val(),
        date : $("#create_form_dob").val(),
        csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val()
-     }, function(data, )
+     },
    )
 });
 /*
