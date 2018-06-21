37178d997d1e9e53eaac23de2eb75373fdd967dd
ineffablelucky <kumar.lakshay1991@gmail.com>
2018-06-21 18:31:10 +0530

    delete modal working. fix ajax with backend

--- Ajax_Django/ajax_submit_search_app/static/ajax_submit_search_app/main.js
+++ Ajax_Django/ajax_submit_search_app/static/ajax_submit_search_app/main.js
@@ -1,3 +1,8 @@
+//
+// {% include 'ajax_submit_search_app/update_modal.html' %}
+// {% include "ajax_submit_search_app/delete_modal.html" %}
+//
+
 $(document).on('submit','#create_form', function(e){
     e.preventDefault();
 
@@ -6,16 +11,24 @@ $(document).on('submit','#create_form', function(e){
         date : $("#create_form_dob").val(),
         csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val()
 
-      }, function(data){
-
+      }, function(data) {
          $('.close').trigger('click');
-         $("#table_id").append('<tr><td>'+ data.name + '</td><td>'+ data.date +'</td><td></td><td><button data-toggle="modal" data-target="#myModal-'+ data.id +'">Delete</button></td></tr>{% include "ajax_submit_search_app/delete_modal.html" %}')
-      })
+         $("#table_id").append('<tr><td class="name">'+ data.name + '</td><td class="date">'+ data.date +'<td class="update" data-id='+ data.id + '><button>Update</button></td><td class="delete" data-id=' + data.id +'><button>Delete</button></td>');
+      });
 });
 
 
+$(document).on('click','.delete', function(f) {
+    $('#myModal-DELETE').modal('show');
+    console.log(this)
+    var data_id = $(this).attr("data-id")
+    console.log(data_id)
+    var parent_tag = $(this).parent()
+    console.log(parent_tag)
+})
+
 /*
-$(document).on('submit','#update_form', function(e){
+$(document).on('submit','#update_form', function(e) {
     e.preventDefault();
 
     $.post('',
